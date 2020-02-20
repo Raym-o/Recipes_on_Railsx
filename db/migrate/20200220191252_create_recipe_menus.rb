6909655e07b186e212cc76b61083d86d72246b76
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-20 13:13:41 -0600

    Created Recipe_Menu model

---
+++ db/migrate/20200220191252_create_recipe_menus.rb
@@ -0,0 +1,11 @@
+class CreateRecipeMenus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :recipe_menus do |t|
+      t.references :recipe, null: false, foreign_key: true
+      t.references :menu, null: false, foreign_key: true
+      t.string :restaurant_name
+
+      t.timestamps
+    end
+  end
+end
