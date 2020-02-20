462a78bfe99ddad9586887029290ef1b04c32d40
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-20 17:19:06 -0600

    Filled out seed script and altered model ingredient

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -4,4 +4,5 @@ class Ingredient < ApplicationRecord
   belongs_to :foodgroup
 
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,41 @@
 
 require 'rest-client'
 
+RecipeIngredient.destroy_all
 RecipeMenu.destroy_all
 Recipe.destroy_all
 Menu.destroy_all
+Ingredient.destroy_all
+Foodgroup.destroy_all
 
-counter = 20
-counter.times do
+FOOD_GROUPS = [
+  { name: 'vegetables', suggested_serving: '2 to 3 cups' },
+  { name: 'fruits', suggested_serving: '1.5 to 2 cups' },
+  { name: 'grains', suggested_serving: '5 to 8 ounces' },
+  { name: 'dairy', suggested_serving: '3 cups fat-free or low fat' },
+  { name: 'protein', suggested_serving: '5 to 6.5 ounces' },
+  { name: 'oils', suggested_serving: '5 to 7 teaspoons' }
+].freeze
+
+FOOD_GROUPS.each do |food|
+  Foodgroup.create(name: food[:name], suggested_serving: food[:suggested_daily_intake])
+end
+
+20.times do
   page_counter = rand(50..100)
   request = RestClient.get("http://www.recipepuppy.com/api/?p=#{page_counter}")
   response = JSON.parse(request)
   data = response['results']
 
+  foodgrp = Foodgroup.first
   data.each do |datum|
-    Recipe.create(name: datum['title'], description: datum['ingredients'])
+    rec = Recipe.create(name: datum['title'], description: datum['ingredients'])
+    array = datum['ingredients'].split(', ')
+    array.each do |index|
+      ing = Ingredient.create(name: index, foodgroup: foodgrp)
+      RecipeIngredient.create(recipe: rec, ingredient: ing) unless ing.id.nil?
+    end
   end
-  page_counter += 1
 end
 
 MENUS = [
@@ -31,12 +51,22 @@ MENUS.each do |mnu|
   Menu.create(name: mnu[:name], description: mnu[:description])
 end
 
-# MENUS.each do |mnu|
-#   all_recipes = Recipe.all
-#   all_recipes.each do |rcp|
-#     RecipeMenu.create(rcp[:id], mnu[:id]) if rcp[:name].include? mnu[:name]
-#   end
-# end
+default_menu = Menu.where(name: 'General').first
+Menu.all.each do |mnu|
+  all_recipes = Recipe.all
+  all_recipes.each do |rcp|
+    if rcp.name.include?(mnu.name)
+      current_menu = Menu.find(mnu[:id])
+      RecipeMenu.create(recipe: rcp, menu: current_menu)
+    else
+      RecipeMenu.create(recipe: rcp, menu: default_menu)
+    end
+  end
+end
 
-puts Menu.first.inspect
-puts Recipe.first.inspect
+puts "Created #{Recipe.count} Recipes"
+puts "Created #{Ingredient.count} Ingredients"
+puts "Created #{Menu.count} Menus"
+puts "Created #{Foodgroup.count} Foodgroups"
+puts "Created #{RecipeIngredient.count} RecipeIngredients"
+puts "Created #{RecipeMenu.count} RecipeMenus"
