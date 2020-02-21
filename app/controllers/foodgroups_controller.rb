bfdffc13e1274a13a5bb6207fc6eb57f105bb42a
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-21 15:20:44 -0600

    Added show/index functions to controllers and views, configured routes for menus, foodgroups, ingredients

--- app/controllers/foodgroups_controller.rb
+++ app/controllers/foodgroups_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class FoodgroupsController < ApplicationController
   def index
+    @foodgroups = Foodgroup.all
   end
 
   def show
+    @foodgroup = Foodgroup.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,30 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # GET /ingredients/ => ingredients#index
   get 'ingredients/index'
-  get 'ingredients/show'
-  get 'menus/index'
-  get 'menus/show'
-  get 'foodgroups/index'
-  get 'foodgroups/show'
 
-  # GET / => recipes#index
+  # GET /ingredients/1 => ingredients#show
+  get 'ingredients/:id', to: 'ingredients#show', id: /\d+/
+
+  # GET /menus/ => menus#index
+  get 'menus/index'
+
+  # GET /menus/1 => menus#show
+  get 'menus/:id', to: 'menus#show', id: /\d+/
+
+  # GET /foodgroups/ => foodgroups#index
+  get 'foodgroups/index'
+
+  # GET /foodgroups/1 => foodgroups#show
+  get 'foodgroups/:id', to: 'foodgroups#show', id: /\d+/
+
+  # GET /recipes/ => recipes#index
   get 'recipes/index'
 
+  # GET /recipes/1 => recipes#show
   get 'recipes/:id', to: 'recipes#show', id: /\d+/
 
+  # GET / => welcome#show
   root 'welcome#index'
 end
