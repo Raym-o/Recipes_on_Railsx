b22869f5e05dc18622221d60af9c08e373125fd7
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-21 15:34:30 -0600

    Added missing relationships between Recipes, Ingredients, and RecipeIngredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,8 @@
 
 class Ingredient < ApplicationRecord
   belongs_to :foodgroup
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
 
   validates :name, presence: true
   validates :name, uniqueness: true
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,5 +3,7 @@
 class Recipe < ApplicationRecord
   has_many :recipe_menus
   has_many :menus, through: :recipe_menus
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
   validates :name, presence: true
 end
