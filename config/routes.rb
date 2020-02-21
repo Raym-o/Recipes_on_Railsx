5990447d1c39c86d83482d2be82693542b783986
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-20 21:52:21 -0600

    Generated ingredients controller with index/show action/view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'ingredients/index'
+  get 'ingredients/show'
   get 'menus/index'
   get 'menus/show'
   get 'foodgroups/index'
