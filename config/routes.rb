836590a6cef424929b19f8683a3c9292d24885e6
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-21 19:06:49 -0600

    Added link to #show to each item in the #index view of each of the remaining visible models.

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,19 @@ Rails.application.routes.draw do
   get 'ingredients/index'
 
   # GET /ingredients/1 => ingredients#show
-  get 'ingredients/:id', to: 'ingredients#show', id: /\d+/
+  get 'ingredients/:id', to: 'ingredients#show', id: /\d+/, as: 'ingredient'
 
   # GET /menus/ => menus#index
   get 'menus/index'
 
   # GET /menus/1 => menus#show
-  get 'menus/:id', to: 'menus#show', id: /\d+/
+  get 'menus/:id', to: 'menus#show', id: /\d+/, as: 'menu'
 
   # GET /foodgroups/ => foodgroups#index
   get 'foodgroups/index'
 
   # GET /foodgroups/1 => foodgroups#show
-  get 'foodgroups/:id', to: 'foodgroups#show', id: /\d+/
+  get 'foodgroups/:id', to: 'foodgroups#show', id: /\d+/, as: 'foodgroup'
 
   # GET /recipes/ => recipes#index
   get 'recipes/index'
