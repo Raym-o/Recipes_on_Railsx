36fd2ac89dc65b434be075d0be1624733357e833
Raymond Weiss <rweiss40@academic.rrc.ca>
2020-02-21 19:22:34 -0600

    Added 'back' link from foodgroup to foodgroups, should be a partial rather than in each view. Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,25 +2,25 @@
 
 Rails.application.routes.draw do
   # GET /ingredients/ => ingredients#index
-  get 'ingredients/index'
+  get 'ingredients/index', as: 'ingredients'
 
   # GET /ingredients/1 => ingredients#show
   get 'ingredients/:id', to: 'ingredients#show', id: /\d+/, as: 'ingredient'
 
   # GET /menus/ => menus#index
-  get 'menus/index'
+  get 'menus/index', as: 'menus'
 
   # GET /menus/1 => menus#show
   get 'menus/:id', to: 'menus#show', id: /\d+/, as: 'menu'
 
   # GET /foodgroups/ => foodgroups#index
-  get 'foodgroups/index'
+  get 'foodgroups/index', as: 'foodgroups'
 
   # GET /foodgroups/1 => foodgroups#show
   get 'foodgroups/:id', to: 'foodgroups#show', id: /\d+/, as: 'foodgroup'
 
   # GET /recipes/ => recipes#index
-  get 'recipes/index'
+  get 'recipes/index', as: 'recipes'
 
   # GET /recipes/1 => recipes#show
   get 'recipes/:id', to: 'recipes#show', id: /\d+/, as: 'recipe'
